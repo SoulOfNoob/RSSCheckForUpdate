8be485c57176172a1e5469cdc0eb8aff34d0a54b
Jan R. <SoulOfNoob@users.noreply.github.com>
2025-10-29 17:44:40 +0100

    Refactor server update script for container handling

--- UnRAID/UpdatePterodactylServersFromRSS .sh
+++ UnRAID/UpdatePterodactylServersFromRSS .sh
@@ -4,7 +4,7 @@ UNRAID_SCRIPT_LOCATION="/mnt/user/SSD/scripts/RSSCheckForUpdate"
 
 # Name;GameID;ContainerID
 server=(
-    'Satisfactory;526870;bc5fe497'
+    'Satisfactory;526870;f869f8e6'
     'ARK;346110;f92600c9'
 )
 
@@ -13,7 +13,7 @@ PROJECT_DIR=$(dirname "$SCRIPT_DIR")
 
 UNRAID=false
 NODE_SCRIPT_LOCATION="${PROJECT_DIR}/js/app.js"
-if [ ! -f $NODE_SCRIPT_LOCATION ]; then
+if [ ! -f "$NODE_SCRIPT_LOCATION" ]; then
 	NODE_SCRIPT_LOCATION="${UNRAID_SCRIPT_LOCATION}/js/app.js"
     if [ ! -f $NODE_SCRIPT_LOCATION ]; then
 	    echo "RSS Script not found"
@@ -23,33 +23,39 @@ if [ ! -f $NODE_SCRIPT_LOCATION ]; then
 	fi
 fi
 
-for server in ${server[@]}; do
+for server in "${server[@]}"; do
 	IFS=";" read -r -a arr <<< "${server}"
 	NAME="${arr[0]}"
 	GAME_ID="${arr[1]}"
-	CONTAINER_ID="${arr[2]}"
+	CONTAINER_NAME="${arr[2]}"
+	CONTAINER_ID=""
 	# echo "NAME: ${NAME}"
 	# echo "GAME_ID: ${GAME_ID}"
-	# echo "CONTAINER_ID: ${CONTAINER_ID}"
+	# echo "CONTAINER_NAME: ${CONTAINER_NAME}"
 
 	RSS_URL="https://steamcommunity.com/games/${GAME_ID}/rss/"
 	# echo "RSS_URL: ${RSS_URL}"
 
-	UPDATE_NEEDED=`node $NODE_SCRIPT_LOCATION $RSS_URL $CONTAINER_ID`
+	UPDATE_NEEDED=$(node "$NODE_SCRIPT_LOCATION" "$RSS_URL" "$CONTAINER_NAME")
 
 	echo "${NAME} - UPDATE_NEEDED: ${UPDATE_NEEDED}"
 
     if [ "$UPDATE_NEEDED" = true ] ; then
-	    echo "Restart ${NAME} Server with ID: ${CONTAINER_ID}"
-	    if $UNRAID; then 
-		    if docker restart $(docker ps | grep $CONTAINER_ID | awk '{print $1}'); then
-				echo "Restarting.."
-				if $UNRAID; then /usr/local/emhttp/webGui/scripts/notify -s "Restarting ${NAME} Server" -d "${CONTAINER_ID}" -e "UpdatePterodactylServersFromRSS" -i "normal"; fi
+	    echo "Try to restart ${NAME} Server with ID: ${CONTAINER_NAME}"
+		CONTAINER_ID="$(docker ps | grep "$CONTAINER_NAME" | awk '{print $1}')" && echo "$CONTAINER_ID"
+		if [ -z "$CONTAINER_ID" ]; then
+			echo "Error: Container ID for ${NAME} (${CONTAINER_NAME}) not found. Exiting."
+			if $UNRAID; then /usr/local/emhttp/webGui/scripts/notify -s "Container Not Found" -d "Server: ${NAME} Container: ${CONTAINER_NAME}" -e "UpdatePterodactylServersFromRSS" -i "warning"; fi
+		else
+			if docker restart "$CONTAINER_ID"; then
+				echo "Restarted!"
+				if $UNRAID; then /usr/local/emhttp/webGui/scripts/notify -s "Restarted ${NAME} Server" -d "Container: [${CONTAINER_ID}]" -e "UpdatePterodactylServersFromRSS" -i "normal"; fi
 			else
-				echo "Restart Failed.."
-				if $UNRAID; then /usr/local/emhttp/webGui/scripts/notify -s "Restarting ${NAME} Server Failed" -d "" -e "UpdatePterodactylServersFromRSS" -i "alert"; fi
+				echo "Restart Failed!"
+				if $UNRAID; then /usr/local/emhttp/webGui/scripts/notify -s "Restarting ${NAME} Server Failed" -d "Container: [${CONTAINER_ID}]" -e "UpdatePterodactylServersFromRSS" -i "warning"; fi
 			fi
 		fi
+		
 	fi
 done
 
